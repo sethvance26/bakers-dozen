f073477a4c42bbb86cc7a5b1bb03f002ef1f885e
Seth Vance <76290048+sethvance26@users.noreply.github.com>
2026-08-27 22:14:05 -0400

    adding saveSelection function, click handler, adding styling

--- script.js
+++ script.js
@@ -3,3 +3,22 @@ const products = [
     "Pastries",
     "Cakes"
 ]
+
+function saveSelection() {
+    const favoriteProduct= document.getElementById("favorite-product");
+    const savedProduct = document.getElementById("saved-product");
+
+    const selectedProduct = favoriteProduct.value;
+
+    if (selectedProduct) {
+        localStorage.setItem("favoriteProduct", selectedProduct);
+        savedProduct.textContent = `We love ${selectedProduct} just as much as you do!`
+    }
+
+}
+
+const saveButton = document.getElementById("save-selection");
+
+if (saveButton) {
+    saveButton.addEventListener("click", saveSelection)
+}
