6fd57ad380406b42d842fe9c7eacc7cd041210c7
Seth Vance <76290048+sethvance26@users.noreply.github.com>
2026-08-29 21:15:14 -0400

    adding name and email validation through validateInquiry function

--- script.js
+++ script.js
@@ -33,3 +33,37 @@ function loadSelection() {
 }
 
 loadSelection();
+
+function validateInquiry(event) {
+    const name = document.getElementById("name");
+    const email = document.getElementById("email");
+
+    const nameError = document.getElementById("name-error");
+    const emailError = document.getElementById("email-error");
+
+    let isValid = true;
+    
+    if (name.value.trim() === "") {
+        nameError.textContent = "Please enter your name";
+        isValid = false;
+        } else {
+            nameError.textContent = ""
+        }
+
+    if(!email.value.includes("@")) {
+        emailError.textContent = "Please enter a valid email address";
+        isValid = false;
+    } else {
+        emailError.textContent = "";
+    }
+
+    if (!isValid) {
+        event.preventDefault();
+    }
+}
+
+const contactForm = document.querySelector("form");
+
+if (contactForm) {
+    contactForm.addEventListenerListener("submit", validateInquiry);
+}
